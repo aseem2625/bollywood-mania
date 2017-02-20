03ea569f4e28cf70666052ff84304a06117fa432
aseem <aseem.g@stayzilla.com>
2017-02-20 06:40:12 +0530

    Update routes

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
-import App from './components/App';
-import Dashboard from './containers/Dashboard';
+import App from './containers/App';
+import Home from './containers/Home';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRedirect to="/home" />
-		<Route path="/home(/**)" component={Dashboard} />
+		<Route path="/home" component={Home} />
 		<Route path="/about" component={About} />
 		<Route path="/*" component={NotFound} />
 	</Route>
